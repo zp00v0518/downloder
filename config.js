112e379a7bfe790cd1fb2ecdaabd4d4b92b3f7df
Dmitriy <you@example.com>
2019-05-11 20:05:52 +0300

    туче

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 const path = require('path')
 module.exports = {
 	url: {
-		base:'https://vs1.coursehunters.net/w4myslf-react-spt/',
+		base:'https://vs3.coursehunters.net/ls-node-fall2018/',
 		changeSection:['lesson', '.mp4']
 	},
-	pathToFolder: path.resolve(__dirname, './React'),
-	fileName: "react",
-	sumFile: 115
+	pathToFolder: path.resolve(__dirname, './NodeJs_Loftschool'),
+	fileName: "loftschool",
+	sumFile: 27
 }
